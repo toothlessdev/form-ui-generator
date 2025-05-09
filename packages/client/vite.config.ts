2e96aebf3a74d00bc5914e395085276bab59d134
toothlessdev <toothless042@gmail.com>
2025-05-09 17:38:51 +0900

    chore: vitest 설정

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -3,6 +3,14 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
+const vitestConfig = {
+    test: {
+        globals: true,
+        environment: "jsdom",
+        setupFiles: "./src/__test__/setup.ts",
+    },
+};
+
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
@@ -11,4 +19,5 @@ export default defineConfig({
             "@": path.resolve(__dirname, "./src"),
         },
     },
+    ...vitestConfig,
 });
